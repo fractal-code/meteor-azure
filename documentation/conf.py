6dd14a7cfa20046d857e09fc723f04925d92505e
Rami Jarrar <rami@talos.email>
2017-06-14 19:50:52 +1000

    Bump version to 2.0.1

--- documentation/conf.py
+++ documentation/conf.py
@@ -18,8 +18,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 project = u'Meteor Azure'
 copyright = u'2017, Talos Technology Pty Ltd'
-version = u'2.0.0'
-release = u'2.0.0'
+version = u'2.0.1'
+release = u'2.0.1'
 
 # -------------------------------
 # Internationalization
